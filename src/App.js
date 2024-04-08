0bc914ed168f8f68ebcb608bed20f0a6f798ffe2
Kevin Torrington <kevin.torrington@accenture.com>
2024-04-08 02:56:48 +0100

    Tweaked slider

--- src/App.js
+++ src/App.js
@@ -20,96 +20,104 @@ function App() {
           {/* Your first panel content */}
 
           <Plot
-            data={[
-              {
-                x: [
-                  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-                  19, 20,
-                ],
-                y: [
-                  2, 9, 2, 2, 3, 2, 2, 2, 16, 2, 2, 2, 3, 2, 2, 12, 13, 2, 2, 1,
-                ],
-                type: "scatter",
-                mode: "lines+markers",
-                name: "Demand (MWh)",
-                marker: { color: "red" },
-              },
-              {
-                x: [
-                  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-                  19, 20,
-                ],
-                y: [2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 1],
-                type: "lines",
-                mode: "lines+markers",
-                name: "Corrected",
-                marker: { color: "green" },
-              },
-            ]}
-            layout={{
-              width: 1300,
-              height: "auto",
-              autosize: true,
-              margin: {
-                l: 50,
-                r: 50,
-                b: 150,
-                t: 50,
-                pad: 4,
-              },
-              legend: {
-                orientation: "h",
-                x: 0.5,
-                xanchor: "center",
-                y: -0.2,
-                yanchor: "bottom",
-              },
-              shapes: [
-                {
-                  type: "rect",
-                  xref: "x",
-                  yref: "paper",
-                  x0: 7.2,
-                  y0: 0,
-                  x1: 10.5,
-                  y1: 1,
-                  fillcolor: "#FF0000",
-                  opacity: 0.04,
-                  line: {
-                    width: 0,
-                  },
-                },
-                {
-                  type: "rect",
-                  xref: "x",
-                  yref: "paper",
-                  x0: 0.5,
-                  y0: 0,
-                  x1: 3.5,
-                  y1: 1,
-                  fillcolor: "#FF0000",
-                  opacity: 0.04,
-                  line: {
-                    width: 0,
-                  },
-                },
-                {
-                  type: "rect",
-                  xref: "x",
-                  yref: "paper",
-                  x0: 14.5,
-                  y0: 0,
-                  x1: 18.5,
-                  y1: 1,
-                  fillcolor: "#FF0000",
-                  opacity: 0.04,
-                  line: {
-                    width: 0,
-                  },
-                },
-              ],
-            }}
-          />
+  data={[
+    {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      y: [2, 9, 2, 2, 3, 2, 2, 2, 16, 2, 2, 2, 3, 2, 2, 12, 13, 2, 2, 1],
+      type: "scatter",
+      mode: "lines+markers",
+      name: "Demand (MWh)",
+      marker: { color: "red" },
+    },
+    {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+      y: [2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 1],
+      type: "lines",
+      mode: "lines+markers",
+      name: "Corrected",
+      marker: { color: "green" },
+    },
+  ]}
+  layout={{
+    width: 1300,
+    height: "auto",
+    autosize: true,
+    xaxis: {
+      rangeselector: {
+       
+      },
+      rangeslider: {
+        thickness: .08,
+        width: 100,
+        bgcolor: "#DFE6ED",
+        borderwidth: 1,
+        bordercolor: "#DFE6ED",
+      },
+    },
+    margin: {
+      l: 50,
+      r: 50,
+      b: 120,
+      t: 50,
+      pad: 4,
+    },
+    legend: {
+      orientation: "h",
+      x: 0.5,
+      xanchor: "center",
+      y: 50,
+      yanchor: "bottom",
+    },
+
+    
+
+    shapes: [
+      {
+        type: 'rect',
+        xref: 'x',
+        yref: 'paper',
+        x0: 7.2,
+        y0: 0,
+        x1: 10.5,
+        y1: 1,
+        fillcolor: '#FF0000',
+        opacity: 0.04,
+        line: {
+          width: 0,
+        },
+      },
+      {
+        type: 'rect',
+        xref: 'x',
+        yref: 'paper',
+        x0: 0.5,
+        y0: 0,
+        x1: 3.5,
+        y1: 1,
+        fillcolor: '#FF0000',
+        opacity: 0.04,
+        line: {
+          width: 0,
+        },
+      },
+      {
+        type: 'rect',
+        xref: 'x',
+        yref: 'paper',
+        x0: 14.5,
+        y0: 0,
+        x1: 18.5,
+        y1: 1,
+        fillcolor: '#FF0000',
+        opacity: 0.04,
+        line: {
+          width: 0,
+        },
+      },
+    ],
+  }}
+/>
+
         </div>
         <div>
           {/* Your second panel content */}
